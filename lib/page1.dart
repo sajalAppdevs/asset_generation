efb47c28ac19611809fa3b039171426f2420eaaa
jakansha2001 <jakansha2001@gmail.com>
2023-11-06 01:38:41 +0530

    feat: :sparkles: dynamically generating assets using flutter_gen package

--- lib/page1.dart
+++ lib/page1.dart
@@ -1,4 +1,5 @@
 import 'package:asset_generation/constants/constants.dart';
+import 'package:asset_generation/gen/assets.gen.dart';
 import 'package:flutter/material.dart';
 
 class Page1 extends StatelessWidget {
@@ -13,7 +14,8 @@ class Page1 extends StatelessWidget {
       body: Center(
         child:
             // Image.asset('assets/dash.png'),
-            Image.asset(Constants.dashImage),
+            // Image.asset(Constants.dashImage),
+            Image.asset(Assets.dash.path),
       ),
     );
   }
